1443ee839810946c74feca146e58fbd88e61ff52
ShivamGupta7795 <shivam.gupta7795@gmail.com>
2017-06-27 11:32:34 -0400

    Random question selection updated
    Load_question function now selects random questions without repetition.

--- app/render.js
+++ app/render.js
@@ -1,60 +1,13 @@
 var questions = [];
-var question_number = -1;
-var questions_shown = 0;
-var max_questions = 0;
-
 var current_points = 0;
 var global_question= null;
 var assumption_selected = [];
 var submit_reasons_set = false;
+var parsed = false;
 
-function display_feedback_for_assumptions(){
-    // window.alert("display_feedback_for_assumptions");
-    var assumptions_ele = global_question["assumptions"];
-    for(i=0;i<assumptions_ele.length;i++){
-        // window.alert(i);
-        var existing_content = document.getElementById("assm_"+i).innerHTML;
-        if(assumptions_ele[i]["assumption_type"]=="needed"){
-           // window.alert("needed"); 
-           // document.getElementById("assm_"+i).innerHTML = existing_content.replace('<br>',' ');
-           // document.getElementById("assm_"+i).innerHTML += "<div class='alert alert-success'><strong>Success!</strong> Indicates a successful or positive action.</div>";
-           // document.getElementById("assm_"+i).innerHTML +="<p>hi</p>"
-           document.getElementById("assm_"+i).style.color = "green"; 
-
-        }
-        else if(assumptions_ele[i]["assumption_type"]=="unneeded"){
-           document.getElementById("assm_"+i).style.color = "red"; 
-
-        }
-        else if(assumptions_ele[i]["assumption_type"]=="complicatingfactor"){
-           document.getElementById("assm_"+i).style.color = "blue"; 
-
-        }
-    }
-}
-
-function check_atleast_one_selected(){
-    var atleast_one_selected = false;
-
-    var checkboxes = document.getElementsByName('all_assumptions_chk_bx');
-    for (i = 0; i < checkboxes.length; i++) {
-        if (checkboxes[i].type == 'checkbox' && checkboxes[i].checked) {
-            atleast_one_selected = true;
-        }   
-   
-    }
-
-    if(atleast_one_selected == false){
-        return false;
-    }
-    return true;
-}
 
 function evaluate_assumptions_submission() {
-    if(!check_atleast_one_selected()){
-        window.alert("Select Assumptions!!!");
-        return false;
-    }
+    document.getElementById('Submit_assm').hidden = true;
     var k = 0; //to populate assumption selected array. Multiple assumptions can be selected. 
     var checkboxes = document.getElementsByName('all_assumptions_chk_bx');
     submit_reasons_set = false;
@@ -81,10 +34,7 @@ function evaluate_assumptions_submission() {
                 //Display reasons
                 var all_reasons = assumptions_ele[assumption_selected[k]]["reasons"];
 
-                if(all_reasons != null){
-                    display_reasons(i,all_reasons);
-                }
-
+                display_reasons(i,all_reasons);
             }
         }
         else{
@@ -96,11 +46,6 @@ function evaluate_assumptions_submission() {
         //k is incremented here so as to be able to use k in between. 
         k++;
     }
-   
-    display_feedback_for_assumptions();
-
-    document.getElementById('Submit_assm').hidden = true;
-
     document.getElementById('score_button_id').innerHTML = "Score: "+current_points;
 
     return false;
@@ -124,7 +69,6 @@ function display_reasons(i,all_reasons){
 }
 
 function evaluate_reasons_submission(){
-
     console.log("evaluate_reasons_submission");
     document.getElementById('submit_reasons').hidden = true;
 
@@ -132,7 +76,6 @@ function evaluate_reasons_submission(){
         var radios = document.getElementsByName('reasons_'+each_assm);
         document.getElementsByName('reasons_'+each_assm).disabled = true;
         for (i = 0; i < radios.length; i++) {
-            radios[i].disabled = true;
             if (radios[i].type == 'radio' && radios[i].checked) {
                 // assumption_selected = radios[i].value;
                 console.log("radio selected:"+radios[i].value);
@@ -178,31 +121,28 @@ function create_checkboxes(assumptions_ele){
 
 }
 
-function select_random_question() {
-    if (questions.length == 0) return false;
-    question_number = Math.floor(questions.length * Math.random());
-    var random_question = questions.splice(question_number, 1)[0];
-    global_question = random_question;
-    return true;
-}
-
-function display_question() {
-    set_questiontitle(global_question.questiontitle);
-    display_assumptions(global_question);
-}
-
 function load_questions() {
     try {
-        var p = new parser(questions_config);
-        questions = p.parse();
-        max_questions = questions.length;
-        questions_shown = 1;
+        if(parsed==false){
+            var p = new parser(questions_config);
+            questions = p.parse();
+            parsed = true;
+        }
+        
         if (questions.length == 0)
-            alert("That file seems to have no questions.");
+            {
+                document.getElementById('next').disabled = true;
+                alert("That file seems to have no questions.");
+            }
         else {
-            select_random_question();
-            set_progress_bar();
-            display_question();
+            var qn = Math.floor(questions.length * Math.random());
+            var random_question = questions[qn];
+            global_question = random_question;
+            // render_question(random_question);
+            // display_ques(random_question);
+            document.getElementById('questiontitle').innerHTML = global_question.questiontitle;
+            display_assumptions(questions[qn]);
+            questions.splice(qn, 1);
         }
     } catch (e) {
         alert("Something went wrong: " + e);
@@ -210,19 +150,7 @@ function load_questions() {
     return false;
 }
 
-function set_questiontitle(t) {
-    document.getElementById('questiontitle').innerHTML = t;
-}
-
-function set_progress_bar(p) {
-    var percent_done = (100 * (questions_shown-1) / max_questions) + '%';
-    if (p) percent_done = p + '%';
-    console.log('width: ' + percent_done);
-    document.getElementById('progbar').style = 'width: ' + percent_done;
-    document.getElementById('readable_percent_done').innerHTML = percent_done + " Complete";
-}
-
-function load_nextquestion() {
+function load_nextquestion(){
    // document.getElementById('question_ele').innerHTML = "Question goes here";
     var rad= document.getElementsByName("selection");
     remove_ele(rad);
@@ -234,18 +162,9 @@ function load_nextquestion() {
     // document.getElementById("score_display_ele").innerHTML = "Score obtained : "+current_points;
     document.getElementById("score_display_reasons").innerHTML= " ";
     document.getElementById("next").disabled= true;
-
-    if (select_random_question()) {
-        questions_shown += 1;
-        set_progress_bar();
-        display_question();
-    }
-    else {
-        document.getElementById('question_images').hidden = true;
-        set_progress_bar(100);
-        set_questiontitle("You've completed all of the questions!");
-    }
+    load_questions();
 }
+
 function remove_ele(ele){
     for(i=0;i<ele.length;i++){
         var id = ele[i].getAttribute('id');
