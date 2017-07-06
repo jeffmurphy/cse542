c0441d19322f68ec3373b72f06a593ebdd663100
ShivamGupta7795 <shivam.gupta7795@gmail.com>
2017-07-05 22:08:48 -0400

    issue 60 resolved.

--- app/render.js
+++ app/render.js
@@ -11,6 +11,7 @@ var submit_reasons_set = false;
 function display_feedback_for_assumptions(){
     // window.alert("display_feedback_for_assumptions");
     var assumptions_ele = global_question["assumptions"];
+    var checkboxes = document.getElementsByName('all_assumptions_chk_bx');
     for(i=0;i<assumptions_ele.length;i++){
         // window.alert(i);
         var existing_content = document.getElementById("assm_"+i).innerHTML;
@@ -23,7 +24,12 @@ function display_feedback_for_assumptions(){
 
         }
         else if(assumptions_ele[i]["assumption_type"]=="unneeded"){
-           document.getElementById("assm_"+i).style.color = "red"; 
+            if(checkboxes[i].checked){
+                document.getElementById("assm_"+i).style.color = "red"; 
+            }else{
+                document.getElementById("assm_"+i).style.color = "#D3D3D3"; 
+            }
+           
 
         }
         else if(assumptions_ele[i]["assumption_type"]=="complicatingfactor"){
@@ -59,10 +65,15 @@ function evaluate_assumptions_submission() {
     var checkboxes = document.getElementsByName('all_assumptions_chk_bx');
     submit_reasons_set = false;
     for (i = 0; i < checkboxes.length; i++) {
+
+        assumption_selected[k] = checkboxes[i].value; // if first checkbox checked then 0 stored in array. 
+        var assumptions_ele = global_question["assumptions"];
+
+
         //process only if the checkbox is checked. else just disable the checkbox.
         if (checkboxes[i].type == 'checkbox' && checkboxes[i].checked) {
-            assumption_selected[k] = checkboxes[i].value; // if first checkbox checked then 0 stored in array. 
-            var assumptions_ele = global_question["assumptions"];
+            // assumption_selected[k] = checkboxes[i].value; // if first checkbox checked then 0 stored in array. 
+            // var assumptions_ele = global_question["assumptions"];
             if(assumptions_ele[assumption_selected[k]]["assumption_type"]=="needed"){
                 document.getElementById("chk_"+i).disabled= true;
                 console.log("assumption type needed"); 
@@ -111,6 +122,9 @@ function display_reasons(i,all_reasons){
     for (j = 0; j < all_reasons.length; j++){
         document.getElementById('assm_'+i).innerHTML += "<input type='radio' name='reasons_"+i+"' value="+j+" id = rd_"+j+" >"+all_reasons[j]["reason_text"]+"<br>";
     }
+
+    document.getElementById("chk_"+i).checked = true;
+
     console.log("Reason checkboxes displayed");
 
     //show submit button as well once all the radios are loaded. 
